3523c3f4d676a6de0e2de777cef1b83d99e606b7
AkshatShukla <aks.shukla47@gmail.com>
2018-06-20 13:56:29 -0400

    Added bearer constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,4 @@
 // export const SAVE = 'SAVE'
 export const clientid = 'OTlmMDIxMjk4MjBlNGM0YjlmNDk4OWQ5OGM1Y2JlODU6MGJhODViNzZjOGY0NDFlNTgwZGI1OTIwMGViYzJjMDA=';
 export const Spotify_Authorization_URL = 'https://accounts.spotify.com/api/token';
+export const Bearer = '';
