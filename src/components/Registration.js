69a244cfd05d4c48247953b4b82a83e7c8386bb7
patelvp <patel.visha@husky.neu.edu>
2018-06-23 17:04:20 -0400

    Added styling to Registration and profile page. Registration Page needs Work

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -8,7 +8,7 @@ const Registration = ({userType, textChanged, selectUserType, registerManager, r
     let selectElement;
 
     const register = () => {
-        if(userType === 'Concert Manager'){
+        if (userType === 'Concert Manager') {
             registerManager(inputElem.value, inputElem1.value, inputElem2.value, selectElement.value, inputElem3.value)
         }
         else
@@ -16,57 +16,57 @@ const Registration = ({userType, textChanged, selectUserType, registerManager, r
     };
 
     return (
-        <div>
-            <form className="form-inline">
-                <label htmlFor="username">Enter username:</label>
-                &nbsp;
-                <input className="col-8"
-                       id="username"
-                       onChange={() => textChanged('username', inputElem.value)}
-                       ref={node => inputElem = node}/>
-            </form>
-            <br/>
-
-            <form className="form-inline">
-                <label htmlFor="password">Enter Password:</label>
-                &nbsp;
-                <input className="col-8"
-                       id="password"
-                       onChange={() => textChanged('password', inputElem1.value)}
-                       ref={node => inputElem1 = node}/>
-            </form>
-            <br/>
-
-            <form className="form-inline">
-                <label htmlFor="verifyPassword">Verify Password:</label>
-                &nbsp;
-                <input className="col-8"
-                       id="verifyPassword"
-                       onChange={() => textChanged('verifyPassword', inputElem2.value)}
-                       ref={node => inputElem2 = node}/>
-            </form>
-
-            <select onChange={() => selectUserType(selectElement.value)}
-                    ref={node => selectElement = node}>
-                <option>Listener</option>
-                <option>Audiophile</option>
-                <option>Concert Manager</option>
-            </select>
-            <br/>
-
-            {userType === 'Concert Manager' && <div>
-                <form className="form-inline">
-                    <label htmlFor="eventLocation">Enter Event Location:</label>
-                    &nbsp;
-                    <input className="col-8"
-                           id="eventLocation"
-                           onChange={() => textChanged('eventLocation', inputElem3.value)}
-                           ref={node => inputElem3 = node}/>
+        <div className="container">
+            <div className="cardRegistration card-container">
+                <form className="form-signin form-inline row m-2">
+                    <label htmlFor="username form-label"  className="col-2 ">Enter username:</label>
+                    <input className="col-9"
+                           id="username"
+                           onChange={() => textChanged('username', inputElem.value)}
+                           ref={node => inputElem = node}/>
                 </form>
-            </div>}
+                <form className="form-signin form-inline m-2">
+                    <label htmlFor="password"  className="col-2 ">Enter Password:</label>
+                    &nbsp;
+                    <input className="col-9"
+                           id="password"
+                           onChange={() => textChanged('password', inputElem1.value)}
+                           ref={node => inputElem1 = node}/>
+                </form>
+                <form className="form-signin form-inline m-2">
+                    <label htmlFor="verifyPassword"  className="col-2 ">Verify Password:</label>
+                    &nbsp;
+                    <input className="col-9"
+                           id="verifyPassword"
+                           onChange={() => textChanged('verifyPassword', inputElem2.value)}
+                           ref={node => inputElem2 = node}/>
+                </form>
+
+                <select onChange={() => selectUserType(selectElement.value)}
+                        ref={node => selectElement = node}>
+                    <option>Listener</option>
+                    <option>Audiophile</option>
+                    <option>Concert Manager</option>
+                </select>
+                <br/>
+
+                {userType === 'Concert Manager' && <div>
+                    <form className="form-inline">
+                        <label htmlFor="eventLocation">Enter Event Location:</label>
+                        &nbsp;
+                        <input className="col-8"
+                               id="eventLocation"
+                               onChange={() => textChanged('eventLocation', inputElem3.value)}
+                               ref={node => inputElem3 = node}/>
+                    </form>
+                </div>}
 
 
-            <button onClick={() => {register()}}>Register</button>
+                <button onClick={() => {
+                    register()
+                }}>Register
+                </button>
+            </div>
         </div>
     )
 }
