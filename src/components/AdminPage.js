993e1112fdcf8aa3cd1ac1dd4d8a3453a56e0f40
patelvp <patel.visha@husky.neu.edu>
2018-06-23 18:44:22 -0400

    Working on admin page

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -5,13 +5,18 @@ export default class AdminPage extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            userid: ''
+        };
         this.loadUsers();
     }
+
+    username;
 
     render() {
         return (
             <div>
-                <table style={{width:'100%'}} className="table">
+                <table style={{width: '100%'}} className="table">
                     <thead>
                     <tr>
                         <th className="col">Username</th>
@@ -22,6 +27,7 @@ export default class AdminPage extends React.Component {
                         <th className="col">email</th>
                         <th className="col">city</th>
                         <th className="col">Phone</th>
+                        <th></th>
                     </tr>
                     </thead>
                     {this.renderUsers()}
@@ -31,18 +37,50 @@ export default class AdminPage extends React.Component {
     }
 
     renderUsers() {
-        return this.props.users.map(user => (
-            <tr>
-                <td>{user.username}</td>
-                <td>{user.password}</td>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.type}</td>
-                <td>{user.email}</td>
-                <td>{user.city}</td>
-                <td>{user.phone}</td>
-            </tr>
-        ))
+        return this.props.users.map(user => {
+            if (this.state.userid === user._id) {
+                return (<tr className="table-active p-3">
+                    <td><input value={this.props.username}
+                               onChange={(Event) => this.username =  Event.target.value}>
+                        {user.username}</input>
+                    </td>
+                    <td>{user.password}</td>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.type}</td>
+                    <td>{user.email}</td>
+                    <td>{user.city}</td>
+                    <td>{user.phone}</td>
+                    <td>
+                        <button className="btn btn-dark" onClick={() => this.UpdateUser(user._id)}>Update</button>
+                    </td>
+                </tr>)
+            }
+            else {
+                return (<tr>
+                    <td>{user.username}</td>
+                    <td>{user.password}</td>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.type}</td>
+                    <td>{user.email}</td>
+                    <td>{user.city}</td>
+                    <td>{user.phone}</td>
+                    <td>
+                        <button className="btn btn-dark" onClick={() => this.SelectUser(user._id)}>Edit</button>
+                    </td>
+                </tr>)
+            }
+
+        })
+    }
+
+    UpdateUser(id) {
+        console.log('In update user', this.username);
+    }
+
+    SelectUser(id) {
+        this.setState({userid: id});
     }
 
     loadUsers() {
