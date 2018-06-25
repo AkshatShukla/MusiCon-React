130ce9ae71a8e0e806033617cde56f0f156bbdd9
AkshatShukla <aks.shukla47@gmail.com>
2018-06-24 21:11:59 -0400

    Fixed minor issues with data not available from API

--- src/components/TicketMasterEventItem.js
+++ src/components/TicketMasterEventItem.js
@@ -8,20 +8,30 @@ const TicketMasterEventItem = ({event}) => {
                 <h5 className="card-title">{event.name}</h5>
                 <p>at {event._embedded.venues[0].name}</p>
                 <form action={event.url}>
-                    <button className="btn btn-outline-dark" style={{marginBottom: '10px'}}>Show on Ticket Master</button>
+                    <button className="btn btn-outline-dark" style={{marginBottom: '10px'}}>Show on Ticket Master
+                    </button>
                 </form>
-                <form action={event.seatmap.staticUrl}>
-                    <button className="btn btn-outline-dark" style={{marginBottom: '10px'}}>Show on Seat Map</button>
-                </form>
+                {event.seatmap !== undefined
+                    ?
+                    <form action={event.seatmap.staticUrl}>
+                        <button className="btn btn-outline-dark" style={{marginBottom: '10px'}}>Show on Seat Map
+                        </button>
+                    </form>
+                    :
+                    <span>&nbsp;</span>}
             </div>
             <div className="card-footer">
                 <small className="text-muted">Concert Date: {event.dates.start.localDate}</small>
                 <br/>
-                <small className="text-muted">Concert Time: {event.dates.start.localTime.substr(0,5)}</small>
+                <small className="text-muted">Concert Time: {event.dates.start.localTime.substr(0, 5)}</small>
             </div>
-            <div className="card-footer">
-                <small className="text-muted">Price Range: USD {event.priceRanges[0].min} - {event.priceRanges[0].max}</small>
-            </div>
+            {event.priceRanges !== undefined
+                ?
+                <div className="card-footer">
+                    <small className="text-muted">Price Range:
+                        USD {event.priceRanges[0].min} - {event.priceRanges[0].max}</small>
+                </div>
+                : <span>&nbsp;</span>}
         </div>
     )
 };
