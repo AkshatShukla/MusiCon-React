ff8ce8bfaa985c0b3020464f29921268fa14f0b2
patelvp <patel.visha@husky.neu.edu>
2018-06-27 22:48:02 -0400

    Changes localhost to hosted nodejs server

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,7 @@ export const SAVE_USERTYPE = 'SAVE_USERTYPE';
 export const SAVE ='SAVE';
 export const LOGOUT ='LOGOUT';
 export const FETCH_PROFILE ='FETCH_PROFILE';
-export const BASE_URL = 'http://localhost:4000/api/';//later change this to heroku url
+export const BASE_URL = 'https://webdev-summer1-project-nodejs.herokuapp.com/api/';//later change this to heroku url
 export const LAST_FM_API_KEY = '936c99c515a1eeafd21e0ca253de20d8';
 export const LAST_FM_TRACK_INFO_URL = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=API_KEY';
 export const LAST_FM_ALBUM_INFO_URL = 'http://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key=API_KEY';
